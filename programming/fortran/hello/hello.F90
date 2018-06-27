2668992fdba60a116cc5202c201d59cc2b03bc6f
thompsonx <tomas.panoc@vsb.cz>
2018-06-27 09:54:10 +0300

    ENH: Fortran ex. 1

--- programming/fortran/hello/hello.F90
+++ programming/fortran/hello/hello.F90
@@ -1,5 +1,19 @@
 program hello
   implicit none
 
+  real :: a = 15
+  real :: b = 20
+
+  logical :: bool = .true.
+
+  character(len=80) :: string='CSC Summer School 2018'
+
   write (*,*) 'Hello world from Fortran!'
+
+  write (*,*) 'Epic computation: ', a + b
+
+  if (bool) then
+    write (*,*) string
+  end if
+
 end program hello
